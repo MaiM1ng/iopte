cd233d3490c81531beb50d2d2af9ab3bf6f88619
MaiM1ng <yebenben001@qq.com>
2025-05-07 05:51:30 +0000

    update package

--- utils/set_packages.sh
+++ utils/set_packages.sh
@@ -2,7 +2,7 @@
 
 sudo apt update
 
-sudo apt install -y flex bison libelf-dev libssl-dev libpci-dev msr-tools sysstat cmake autoconf automake libtool gcc make texinfo
+sudo apt install -y flex bison libelf-dev libssl-dev libpci-dev msr-tools sysstat cmake autoconf automake libtool gcc make texinfo screen neofetch
 
 sudo apt install -y python3-pip
 
